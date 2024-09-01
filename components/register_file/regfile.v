142f3932108fd9a6b009b9f4927ffe1586134378
Kavan Heppenstall <kavan.heppenstall@student.manchester.ac.uk>
2024-09-01 21:05:49 +0100

    Regfile quick fix
    Regfile write enable hotfix

--- components/register_file/regfile.v
+++ components/register_file/regfile.v
@@ -5,9 +5,9 @@
 `include "../register/register.v"
 `include "../line_decoder/decoder.v"
 
-module regfile(A, clk, w_in, w_en, reset, op0, op1);
+module regfile(DEST, SRC0, SRC1, clk, w_in, w_en, reset, op0, op1);
 
-input [15:0] A;
+input [2:0] DEST, SRC0, SRC1;
 
 input [15:0] w_in;
 
@@ -21,20 +21,20 @@ wire [15:0] in0, in1, in2, in3, in4, in5, in6, in7;
 
 wire [15:0] out0, out1, out2, out3, out4, out5, out6, out7;
 
-decoder3to8 D0(.A(A[11:9]), .en(1'b1), .D(reg_en));
+decoder3to8 D0(.A(DEST), .en(1'b1), .D(reg_en));
 
-demultiplexer8bit DMUX0(.A(w_in), .sel(A[11:9]), .Q(in0), .R(in1), .S(in2), .T(in3), .U(in4), .V(in5), .W(in6), .X(in7));
+//demultiplexer8bit DMUX0(.A(w_in), .sel(A[11:9]), .Q(in0), .R(in1), .S(in2), .T(in3), .U(in4), .V(in5), .W(in6), .X(in7));
 
-register16bit R0(.D(in0), .clk(clk), .en(reg_en[0]), .reset(reset), .Q(out0));
-register16bit R1(.D(in1), .clk(clk), .en(reg_en[1]), .reset(reset), .Q(out1));
-register16bit R2(.D(in2), .clk(clk), .en(reg_en[2]), .reset(reset), .Q(out2));
-register16bit R3(.D(in3), .clk(clk), .en(reg_en[3]), .reset(reset), .Q(out3));
-register16bit R4(.D(in4), .clk(clk), .en(reg_en[4]), .reset(reset), .Q(out4));
-register16bit R5(.D(in5), .clk(clk), .en(reg_en[5]), .reset(reset), .Q(out5));
-register16bit R6(.D(in6), .clk(clk), .en(reg_en[6]), .reset(reset), .Q(out6));
-register16bit R7(.D(in7), .clk(clk), .en(reg_en[7]), .reset(reset), .Q(out7));
+register16bit R0(.D(w_in), .clk(clk), .en(reg_en[0] & w_en), .reset(reset), .Q(out0));
+register16bit R1(.D(w_in), .clk(clk), .en(reg_en[1] & w_en), .reset(reset), .Q(out1));
+register16bit R2(.D(w_in), .clk(clk), .en(reg_en[2] & w_en), .reset(reset), .Q(out2));
+register16bit R3(.D(w_in), .clk(clk), .en(reg_en[3] & w_en), .reset(reset), .Q(out3));
+register16bit R4(.D(w_in), .clk(clk), .en(reg_en[4] & w_en), .reset(reset), .Q(out4));
+register16bit R5(.D(w_in), .clk(clk), .en(reg_en[5] & w_en), .reset(reset), .Q(out5));
+register16bit R6(.D(w_in), .clk(clk), .en(reg_en[6] & w_en), .reset(reset), .Q(out6));
+register16bit R7(.D(w_in), .clk(clk), .en(reg_en[7] & w_en), .reset(reset), .Q(out7));
 
-multiplexer8to2 MUX0(.A(out0), .B(out1), .C(out2), .D(out3), .E(out4), .F(out5), .G(out6), .H(out7), .sel0(A[8:6]), .sel1(A[5:3]), .Q0(op0), .Q1(op1));
+multiplexer8to2 MUX0(.A(out0), .B(out1), .C(out2), .D(out3), .E(out4), .F(out5), .G(out6), .H(out7), .sel0(SRC0), .sel1(SRC1), .Q0(op0), .Q1(op1));
 
 
 endmodule
--- components/register_file/regfile_tb.v
+++ components/register_file/regfile_tb.v
@@ -13,7 +13,7 @@ reg clk;
 
 // I/Os
 
-reg [15:0] A;
+reg [2:0] DEST, SRC0, SRC1;
 
 reg [15:0] w_in;
 
@@ -21,7 +21,7 @@ reg w_en, reset;
 
 wire [15:0] op0, op1;
 
-regfile dut(.A, .clk, .w_in, .w_en, .reset, .op0, .op1);
+regfile dut(.DEST(DEST), .SRC0(SRC0), .SRC1(SRC1), .clk, .w_in, .w_en, .reset, .op0, .op1);
 
 
 initial begin
@@ -47,9 +47,15 @@ end
 initial begin // Test Cases
     
     #20;
-    A = 16'h0000; w_in = 16'h0000; w_en = 1'b0; reset = 1'b0; #20;
-    A = 16'h1048; w_in = 16'h0000; w_en = 1'b1; reset = 1'b0; #20;
-    A = 16'hFF4F; w_in = 16'hEEEE; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b000; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'h0000; w_en = 1'b0; reset = 1'b0; #20;
+    DEST = 3'b001; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hAAAA; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b010; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hBBBB; w_en = 1'b0; reset = 1'b0; #20;
+    DEST = 3'b011; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hCCCC; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b100; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hDDDD; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b101; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hEEEE; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b110; SRC0 = 3'b000; SRC1 = 3'b000; w_in = 16'hFFFF; w_en = 1'b1; reset = 1'b0; #20;
+    DEST = 3'b000; SRC0 = 3'b010; SRC1 = 3'b100; w_in = 16'hEEEE; w_en = 1'b0; reset = 1'b0; #20;
+    DEST = 3'b000; SRC0 = 3'b110; SRC1 = 3'b100; w_in = 16'hEEEE; w_en = 1'b0; reset = 1'b0; #20;
 
 
     $display("end simulation");
