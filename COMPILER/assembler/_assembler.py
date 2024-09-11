43b1a960f94fb8e7221d4cd90fb4f64455bb82fe
Kavan Heppenstall <kavan.heppenstall@student.manchester.ac.uk>
2024-09-11 12:00:56 +0100

    Assembler
    Create and assembler to turn custom assembly code into custom ISA instructions

---
+++ COMPILER/assembler/_assembler.py
@@ -0,0 +1,139 @@
+# Created by Kavan Heppenstall, 11/09/2024
+
+class Assembler:
+
+    def __init__(self, instructions):
+        self._op_map = {
+            "JMP" : "0000",
+            "ADD" : "0001",
+            "SUB" : "0010",
+            "LSL" : "0011",
+            "LSR" : "0100",
+            "AND" : "0101",
+            "OR" : "0110",
+            "XOR" : "0111",
+            "LD" : "1000",
+            "ST" : "1001",
+            "MOV" : "1010",
+            "BEQ" : "1011",
+            "BNE" : "1100",
+            "BLT" : "1101",
+            "CMP" : "1111"
+        }
+        self._label_map = {}
+        self._instructions = instructions
+        self._pass = False
+
+    def assemble(self):
+
+        if not self._pass:
+            self.find_labels()
+            self._pass = True
+        return self.generate_machine_code()
+
+    def find_labels(self):
+        address = 0
+
+        for instruction in self._instructions:
+            if ':' in instruction:
+                label = instruction.split(':')[0]
+                self._label_map[label] = address
+            else:
+                address += 1
+
+    def generate_machine_code(self):
+        
+        m_code = []
+        address = 0
+        for instruction in self._instructions:
+            instruction = instruction.strip()
+            instruction = instruction.replace('[', '')
+            instruction = instruction.replace(']', '')
+            if ':' in instruction:
+                continue
+            elif 'JMP' in instruction:
+                label = instruction.split(' ')[1]
+                if label in self._label_map:
+                    code = self._op_map['JMP'] + self.get_address_code(self._label_map[label])
+                else:
+                    raise ValueError(f"Undefined label {label}")
+            elif 'ADD' in instruction or 'SUB' in instruction or 'LSL' in instruction or 'LSR' in instruction or 'AND' in instruction or 'OR' in instruction or 'XOR' in instruction or 'CMP' in instruction:
+                parts = instruction.split(' ')
+
+                OP = self._op_map[parts[0][0:3]]
+                REG0 = self.get_reg_code(parts[1])
+                REG1 = self.get_reg_code(parts[2])
+
+                immed_en = '0'
+                flag_en = '0'
+
+                if 'S' in parts[0][-1]:
+                    flag_en = '1'
+                
+                if '#' in parts[3]:
+                    immed_en = '1'
+                    code = OP + REG0 + REG1 + self.get_immed_code(parts[3].strip('#')) + flag_en + immed_en
+                else:
+                    REG2 = self.get_reg_code(parts[3])
+                    code = OP + REG0 + REG1 + REG2 + '0' + flag_en + immed_en
+            
+            elif 'LD' in instruction or 'ST' in instruction:
+                parts = instruction.split(' ')
+
+                OP = self._op_map[parts[0][0:2]]
+                REG0 = self.get_reg_code(parts[1])
+                REG1 = self.get_reg_code(parts[2])
+
+                immed_en = '0'
+                flag_en = '0'
+
+                if 'S' in parts[0][-1]:
+                    flag_en = '1'
+                
+                if '#' in parts[3]:
+                    immed_en = '1'
+                    code = OP + REG0 + REG1 + self.get_immed_code(parts[3].strip('#')) + flag_en + immed_en
+                else:
+                    REG2 = self.get_reg_code(parts[3])
+                    code = OP + REG0 + REG1 + REG2 + '0' + flag_en + immed_en
+            elif 'MOV' in instruction:
+                parts = instruction.split(' ')
+
+                OP = self._op_map[parts[0][0:3]]
+                REG0 = self.get_reg_code(parts[1])
+                REG1 = self.get_reg_code(parts[2])
+
+                immed_en = '0'
+                flag_en = '0'
+
+                if 'S' in parts[0][-1]:
+                    flag_en = '1'
+                
+                if '#' in parts[3]:
+                    immed_en = '1'
+                    code = OP + REG0 + REG1 + self.get_immed_code(parts[3].strip('#')) + flag_en + immed_en
+                else:
+                    raise ValueError("MOV instruction missing immediate")
+            elif 'BEQ' in instruction or 'BNE' in instruction or 'BLT' in instruction:
+                label = instruction.split(' ')[1]
+                if label in self._label_map:
+                    code = self._op_map['JMP'] + self.get_address_code(self._label_map[label])
+                else:
+                    raise ValueError(f"Undefined label {label}")
+
+            m_code.append(code)
+            address += 1
+        return m_code
+
+    def get_reg_code(self, reg):
+        return format(int(reg[1]), '03b')
+
+    def get_immed_code(self, immed):
+        if int(immed) > 15:
+            raise ValueError("Immediate > 15 unsupported")
+        elif int(immed) < 0:
+            raise ValueError("Immediate < 0 unsupported")
+        return format(int(immed), '04b')
+
+    def get_address_code(self, address):
+        return format(address, '012b')
--- COMPILER/main.py
+++ COMPILER/main.py
@@ -5,6 +5,7 @@ import sys
 from parser import _parser as parser
 from lexer import _lexer as lexer
 from generator import _generator as generator
+from assembler import _assembler as assembler
 
 t0 = lexer.Tokenizer()
 t0.Tokenize(sys.argv)
@@ -13,6 +14,14 @@ p0 = parser.Parser(t0.tokens)
 
 g0 = generator.Generator(p0.program)
 
-assem = g0.get_assembly()
+print(g0.output)
 
-print(assem)
+#a0 = assembler.Assembler(g0.output)
+
+a0 = assembler.Assembler(['lab:','ADDS R0 R0 #13', 'ADDS R0 R0 #13', 'ST R0 [R7 #0]', 'LD R1 [R7 #0]', 'ADDS R1 R1 R0', 'foo:', 'CMP R0 R0 R0', 'BEQ foo'])
+
+machine_code = a0.assemble()
+
+for i in range(0, len(machine_code)):
+    machine_code[i] = f"{int(machine_code[i], 2):04X}"
+    print(machine_code[i])  # Convert binary to hexadecimal
